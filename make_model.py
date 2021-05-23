ec6351bb602e7f95a32410527ccafd81254817f7
DESKTOP-QQ3J3NT\kimhy <kimhyun98@ajou.ac.kr>
2021-05-23 20:52:26 +0900

    implement model

--- make_model.py
+++ make_model.py
@@ -1,11 +1,10 @@
 import os
 import numpy as np
-from keras.layers.normalization_v2 import BatchNormalization
-from keras_preprocessing.image import load_img, img_to_array
 import tensorflow as tf
-from tensorflow.python.keras.applications.resnet import preprocess_input, ResNet50
-from tensorflow.python.keras.layers import Dense, Flatten
-from tensorflow.python.keras.models import Sequential
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, BatchNormalization
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 mask_path = "./mask/"
 nomask_path = "./nomask/"
@@ -46,35 +45,14 @@ training_label = label[0: int(0.8 * total_len)]
 validation_img = total_img[int(0.8 * total_len):, :, :, :]
 validation_label = label[int(0.8 * total_len):]
 
-# %%
-# create the base pre-trained model
-IMG_SHAPE = (224, 224, 3)
-
-base_model = ResNet50(input_shape=IMG_SHAPE, weights='imagenet', include_top=False)
-base_model.trainable = False
-base_model.summary()
-print("Number of layers in the base model: ", len(base_model.layers))
-
-flatten_layer = Flatten()
-dense_layer1 = Dense(128, activation='relu')
-bn_layer1 = BatchNormalization()
-dense_layer2 = Dense(1, activation=tf.nn.sigmoid)
-
-model = Sequential([
-    base_model,
-    flatten_layer,
-    dense_layer1,
-    bn_layer1,
-    dense_layer2,
-])
-
-base_learning_rate = 0.001
-model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+model = ResNet50(input_shape=(224, 224, 3), include_top=False)
+layer1 = Dense(128, input_dim=1,  activation='relu')
+layer2 = Dense(1, activation='sigmoid')
+model = Sequential([model, layer1, layer2])
+model.summary()
+model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
-model.summary()
 
 model.fit(training_img, training_label, epochs=10, batch_size=16, validation_data=(validation_img, validation_label))
-
-# save model
-model.save("model.h5")
+model.save("mask_detector.h5")
